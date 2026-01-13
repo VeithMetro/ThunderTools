b2e51203d16aa9423d7a31771114cb15b08996ce
Mateusz Daniluk <121170681+VeithMetro@users.noreply.github.com>
2026-01-13 09:45:41 +0100

    Update note message in PluginSkeletonGenerator

--- PluginSkeletonGenerator/PluginSkeletonGenerator.py
+++ PluginSkeletonGenerator/PluginSkeletonGenerator.py
@@ -5,7 +5,7 @@ from menu.Menu import menu
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 def main():
-    print('[NOTE]: The output from this generator is a skeleton. Please correct the generated methods accordingly!')
+    print('[NOTE]: TEST The output from this generator is a skeleton. Please correct the generated methods accordingly!')
     print('[NOTE]: This generator does not include all the options available for a plugin! ')
     menu()
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
